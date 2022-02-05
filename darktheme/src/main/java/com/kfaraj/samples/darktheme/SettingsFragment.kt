db5efd7373096d6059d6095f4c0b75513dc3cdaa
Kamal Faraj <kfaraj.dev@gmail.com>
2022-02-05 18:08:24 +0100

    Update dependencies

--- darktheme/src/main/java/com/kfaraj/samples/darktheme/SettingsFragment.kt
+++ darktheme/src/main/java/com/kfaraj/samples/darktheme/SettingsFragment.kt
@@ -18,12 +18,12 @@ class SettingsFragment : PreferenceFragmentCompat(),
 
     override fun onResume() {
         super.onResume()
-        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
     }
 
     override fun onPause() {
         super.onPause()
-        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
