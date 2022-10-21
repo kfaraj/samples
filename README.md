32c63253a582fe2c91cf58f99cf353696f64207c
Kamal Faraj <kfaraj.dev@gmail.com>
2022-10-21 17:42:56 +0200

    Update samples

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This project is a collection of samples for Android.
 This sample app demonstrates how to implement a
 [dark theme](https://developer.android.com/guide/topics/ui/look-and-feel/darktheme).
 
-![Dark theme sample app on a Pixel 2](screenshots/darktheme.png)
+![Dark theme sample app on a Pixel 2](screenshots/darktheme.webp)
--- darktheme/src/main/java/com/kfaraj/samples/darktheme/SettingsActivity.kt
+++ darktheme/src/main/java/com/kfaraj/samples/darktheme/SettingsActivity.kt
@@ -1,28 +1,26 @@
 package com.kfaraj.samples.darktheme
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.google.android.material.appbar.MaterialToolbar
 
 /**
  * Contains settings.
  */
-class SettingsActivity : AppCompatActivity(R.layout.activity_settings) {
+class SettingsActivity : AppCompatActivity(R.layout.activity_settings),
+    OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val toolbar = ActivityCompat.requireViewById<MaterialToolbar>(this, R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                replace<SettingsFragment>(R.id.container)
-                setReorderingAllowed(true)
-            }
-        }
+        toolbar.setNavigationOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        onNavigateUp()
     }
 
 }
