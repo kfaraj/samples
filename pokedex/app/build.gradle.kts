d3b1643e2205903a27296edb9c73d52558003bfd
Kamal Faraj <kfaraj.dev@gmail.com>
2025-03-01 17:58:05 +0100

    Remove support for Android Marshmallow

--- pokedex/app/build.gradle.kts
+++ pokedex/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
     defaultConfig {
         applicationId = "com.kfaraj.samples.pokedex"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "0.0.1"
