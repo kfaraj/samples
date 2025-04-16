452ce485fad975a02d7c3bbfa86e9dd133d9adfc
Kamal Faraj <kfaraj.dev@gmail.com>
2025-04-16 14:43:12 +0200

    Increase version

--- darktheme/app/build.gradle.kts
+++ darktheme/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.1.0"
         vectorDrawables {
             useSupportLibrary = true
         }
--- pokedex/app/build.gradle.kts
+++ pokedex/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
