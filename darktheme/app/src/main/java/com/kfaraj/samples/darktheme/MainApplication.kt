fc5ea5aed8f5b78c78102f9e8a74945d5475cdf4
Kamal Faraj <kfaraj.dev@gmail.com>
2025-08-14 14:57:04 +0200

    Update architecture

--- darktheme/app/src/main/java/com/kfaraj/samples/darktheme/MainApplication.kt
+++ darktheme/app/src/main/java/com/kfaraj/samples/darktheme/MainApplication.kt
@@ -1,23 +1,44 @@
 package com.kfaraj.samples.darktheme
 
 import android.app.Application
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import androidx.appcompat.app.AppCompatDelegate
-import com.kfaraj.samples.darktheme.data.SettingsRepository
-import com.kfaraj.samples.darktheme.domain.GetNightModeUseCase
+import androidx.preference.PreferenceManager
+import com.kfaraj.samples.darktheme.data.Settings
 
 /**
  * Demonstrates how to implement a dark theme.
  */
-class MainApplication : Application() {
-
-    private lateinit var getNightModeUseCase: GetNightModeUseCase
+class MainApplication : Application(),
+    OnSharedPreferenceChangeListener {
 
     override fun onCreate() {
         super.onCreate()
-        val settingsRepository = SettingsRepository.getInstance(this)
-        getNightModeUseCase = GetNightModeUseCase(settingsRepository)
-        val nightMode = getNightModeUseCase()
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, true)
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        prefs.registerOnSharedPreferenceChangeListener(this)
+        onSharedPreferenceChanged(prefs, null)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
+        val nightMode = getNightMode(sharedPreferences)
         AppCompatDelegate.setDefaultNightMode(nightMode)
+    }
+
+    /**
+     * Returns the night mode.
+     */
+    private fun getNightMode(prefs: SharedPreferences): Int {
+        val theme = prefs.getString(
+            Settings.THEME,
+            Settings.THEME_SYSTEM_DEFAULT
+        )
+        return when (theme) {
+            Settings.THEME_LIGHT -> AppCompatDelegate.MODE_NIGHT_NO
+            Settings.THEME_DARK -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
     }
 
 }
