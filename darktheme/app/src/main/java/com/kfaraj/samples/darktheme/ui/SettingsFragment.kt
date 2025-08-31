53e9a8fb03d238b052155833f1a565ccdbbaadad
Kamal Faraj <kfaraj.dev@gmail.com>
2025-08-31 11:21:56 +0200

    Update architecture

--- darktheme/app/src/main/java/com/kfaraj/samples/darktheme/ui/SettingsFragment.kt
+++ darktheme/app/src/main/java/com/kfaraj/samples/darktheme/ui/SettingsFragment.kt
@@ -1,31 +1,16 @@
 package com.kfaraj.samples.darktheme.ui
 
-import android.content.SharedPreferences
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowInsetsCompat
 import androidx.preference.PreferenceFragmentCompat
 import com.kfaraj.samples.darktheme.R
-import com.kfaraj.samples.darktheme.data.SettingsRepository
-import com.kfaraj.samples.darktheme.domain.GetNightModeUseCase
 import com.kfaraj.samples.darktheme.util.applyWindowInsetsPadding
 
 /**
  * Contains settings.
  */
-class SettingsFragment : PreferenceFragmentCompat(),
-    OnSharedPreferenceChangeListener {
-
-    private lateinit var getNightModeUseCase: GetNightModeUseCase
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val context = requireContext()
-        val settingsRepository = SettingsRepository.getInstance(context)
-        getNightModeUseCase = GetNightModeUseCase(settingsRepository)
-    }
+class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -37,21 +22,6 @@ class SettingsFragment : PreferenceFragmentCompat(),
             WindowInsetsCompat.Type.systemBars(),
             applyBottom = true
         )
-    }
-
-    override fun onStart() {
-        super.onStart()
-        preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
-        val nightMode = getNightModeUseCase()
-        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 
 }
