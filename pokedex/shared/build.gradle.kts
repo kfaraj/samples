8107da97315e2d741e1eacb7966256b1879bb9ac
Kamal Faraj <kfaraj.dev@gmail.com>
2026-09-02 16:43:01 +0200

    Add iOS app with shared UI

--- pokedex/shared/build.gradle.kts
+++ pokedex/shared/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     alias(libs.plugins.com.android.kotlin.multiplatform.library)
+    alias(libs.plugins.org.jetbrains.compose)
     alias(libs.plugins.org.jetbrains.kotlin.multiplatform)
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.compose)
+    alias(libs.plugins.io.insert.koin.compiler.plugin)
 }
 
 kotlin {
@@ -11,11 +16,25 @@ kotlin {
     }
     iosArm64()
     iosSimulatorArm64()
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries {
+            framework {
+                baseName = "Shared"
+            }
+        }
+    }
     sourceSets {
         commonMain {
             dependencies {
                 api(project(":pokedex:core:ui"))
                 api(project(":pokedex:feature:pokemon"))
+            }
+        }
+        iosMain {
+            dependencies {
+                implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+                implementation(libs.io.insert.koin.annotations)
+                implementation(libs.io.insert.koin.core)
             }
         }
     }
