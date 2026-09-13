651e7a008485cf2de71d2a927100417dca474d9a
Kamal Faraj <kfaraj.dev@gmail.com>
2026-09-13 16:23:33 +0200

    Update architecture

--- pokedex/feature/pokemon/src/main/kotlin/com/kfaraj/samples/pokedex/feature/pokemon/PokemonListViewModel.kt
+++ pokedex/feature/pokemon/src/main/kotlin/com/kfaraj/samples/pokedex/feature/pokemon/PokemonListViewModel.kt
@@ -2,15 +2,17 @@ package com.kfaraj.samples.pokedex.feature.pokemon
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.ItemSnapshotList
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
+import androidx.paging.asItemSnapshotListFlow
 import androidx.paging.map
 import com.kfaraj.samples.pokedex.data.pokemon.Pokemon
 import com.kfaraj.samples.pokedex.data.pokemon.PokemonRepository
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import org.koin.core.annotation.KoinViewModel
 
 /**
@@ -21,23 +23,47 @@ internal class PokemonListViewModel(
     pokemonRepository: PokemonRepository
 ) : ViewModel() {
 
+    private val pagingConfig = PagingConfig(
+        pageSize = PAGE_SIZE
+    )
+    private val pager = Pager(
+        config = pagingConfig,
+        remoteMediator = pokemonRepository.getRemoteMediator(),
+        pagingSourceFactory = { pokemonRepository.getPagingSource() }
+    )
+
     /**
      * The stream of paged Pokémon list items UI states.
      */
-    val pagingData: Flow<PagingData<PokemonListItemUiState>> =
-        Pager(
-            PagingConfig(PAGE_SIZE),
-            null,
-            pokemonRepository.getRemoteMediator()
-        ) {
-            pokemonRepository.getPagingSource()
-        }.flow
+    val itemSnapshotList: StateFlow<ItemSnapshotList<PokemonListItemUiState>> =
+        pager.flow
             .map { pagingData ->
                 pagingData.map { pokemon ->
                     pokemon.toPokemonListItemUiState()
                 }
             }
-            .cachedIn(viewModelScope)
+            .asItemSnapshotListFlow()
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000L),
+                ItemSnapshotList(0, 0, emptyList())
+            )
+
+    /**
+     * Indicates that an item from the [itemSnapshotList] has been accessed at [index].
+     */
+    fun itemAccessed(itemSnapshotList: ItemSnapshotList<PokemonListItemUiState>, index: Int) {
+        if (index <= itemSnapshotList.placeholdersBefore +
+            pagingConfig.prefetchDistance
+        ) {
+            pager.prepend()
+        }
+        if (index >= itemSnapshotList.size - itemSnapshotList.placeholdersAfter - 1 -
+            pagingConfig.prefetchDistance
+        ) {
+            pager.append()
+        }
+    }
 
     /**
      * Converts the model from the data layer to the UI layer.
